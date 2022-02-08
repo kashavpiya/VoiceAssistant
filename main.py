8d848057b81229b9ad57fb00b226cc6d55446c3f
kashavpiya <71453613+kashavpiya@users.noreply.github.com>
2022-02-08 12:11:22 -0600

    added main

---
+++ main.py
@@ -0,0 +1,51 @@
+import speech_recognition as sr
+import pyttsx3
+import pyaudio
+import pywhatkit
+import datetime
+import wikipedia
+
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print("Listening...")
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'quinn' in command:
+                command = command.replace('quinn', '')
+                print(command)
+
+    except:
+        pass
+    return command
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        print(time)
+        talk("The current time is" + time)
+    elif 'who is' in command:
+        person = command.replace('who is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+
+run_alexa()
