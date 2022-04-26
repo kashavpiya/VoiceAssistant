071035119041878a794e63c7fc585b58cd39cced
srijal <srijaldhar@gmail.com>
2022-04-26 08:42:52 -0500

    this day in history

--- main.py
+++ main.py
@@ -40,6 +40,11 @@ import pandas as pd
 #import requests
 import random
 
+#import for this day in history
+import argparse
+from bs4 import BeautifulSoup
+
+
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
@@ -225,6 +230,8 @@ def run_alexa(command):
         return
 
     #work on the day
+
+
     #fact of the day
 
     elif 'fact' in command:
@@ -233,6 +240,43 @@ def run_alexa(command):
         talk(fact)
 
     #this day in history
+    elif 'today' in command:
+        MONTHS = {1: ('January', 31),
+                  2: ('February', 29),
+                  3: ('March', 31),
+                  4: ('April', 30),
+                  5: ('May', 31),
+                  6: ('June', 30),
+                  7: ('July', 31),
+                  8: ('August', 31),
+                  9: ('September', 30),
+                  10: ('October', 31),
+                  11: ('November', 30),
+                  12: ('December', 31)}
+
+        parser = argparse.ArgumentParser(prog='What Happened On This Day',
+                                         description='Find out what happened on a particular day in history')
+        parser.add_argument('occurrence', nargs='?', default='Holidays_and_observances', type=str,
+                            choices=['Events', 'Births', 'Deaths',
+                                     'Holidays_and_observances'],
+                            help='Name of the occurrence')
+        parser.add_argument('-m', '--month', type=int,
+                            default=datetime.date.today().month, choices=range(1, 13), help='Month to be looked')
+        parser.add_argument('-d', '--day', type=int,
+                            default=datetime.date.today().day, help='Day to be looked')
+        args = parser.parse_args()
+        month_day_tuple = MONTHS[args.month]
+        if not 1 <= args.day <= month_day_tuple[1]:
+            parser.error(
+                f"argument -d/--day: invalid choice: {args.day} (choose from {', '.join(map(repr, range(1, month_day_tuple[1] + 1)))})")
+        else:
+            search_param = f'{month_day_tuple[0]}_{args.day}'
+        dateList = find(search_param, args.occurrence)
+
+        temp = random.choice(dateList)
+        print(temp)
+        talk(remp)
+
 
 
     # gives you a quote
@@ -294,6 +338,17 @@ def give_fun_fact():
 
     return fact
 
+# support function for this day in history
+
+def find(search_param, occurrence):
+    response = requests.get(f'https://en.wikipedia.org/wiki/{search_param}')
+    soup = BeautifulSoup(response.text, 'html.parser')
+    heading = soup.find(id=occurrence)
+    data_list = heading.find_next('ul')
+    factList = []
+    for data in data_list.find_all('li'):
+        factList.append(data.text)
+    return factList
 
 # supports the condition that returns the weather
 def scrape_weather(city):
